245d69a2234e561d63f6ccdd93066a195c8902ef
Vino Rodrigues <github@m.vinorodrigues.com>
2022-05-15 16:09:48 +1000

    add IDOBAO ID42, a.k.a. Abacus

---
+++ keyboards/idobao/id42/keymaps/vial/config.h
@@ -0,0 +1,14 @@
+// Copyright 2022 Vino Rodrigues (@vinorodrigues)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x7D, 0x8B, 0xF8, 0x23, 0x75, 0xAA, 0xEB, 0xA6}
+
+// Trim Vial f/w
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+
+#ifndef VIA_QMK_RGBLIGHT_ENABLE
+    #define VIA_QMK_RGBLIGHT_ENABLE
+#endif
---
+++ keyboards/idobao/id42/keymaps/vial/rules.mk
@@ -0,0 +1,10 @@
+
+LTO_ENABLE = yes
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+
+# Trim Vial f/w
+QMK_SETTINGS = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
